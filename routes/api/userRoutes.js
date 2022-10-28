2144f6cf550c41aa95e1339c5d83ca73ae74b30d
Victoria El Bedewi <victoriaelbedewi@Victorias-Air.askey.com>
2022-10-28 08:57:38 -0700

    user routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+  } = require('../../controllers/userController');
+
+// Set up GET all and POST at /api/users
+router
+  .route('/')
+  .get(getUsers)
+  .post(createUser);
+
+// Set up GET one, PUT, and DELETE at /api/users/:id
+router
+  .route('/:id')
+  .get(getSingleUser)
+  .put(updateUser)
+  .delete(deleteUser);
+
+router
+  .route('/:userId/friends/:friendId')
+  .post(addFriend)
+  .delete(deleteFriend);
+
+module.exports = router;
