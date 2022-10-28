1eacebe3218a0e1539f7296c88e427053c79c10e
Victoria El Bedewi <victoriaelbedewi@Victorias-Air.askey.com>
2022-10-28 08:18:10 -0700

    get single thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,10 +1,21 @@
 const { User, Thought } = require('../models');
 
 module.exports = {
-  // Get all courses
+  // Get all thoughts
   getThoughts(req, res) {
     Thought.find()
       .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Get a thought
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select('-__v')
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought with that ID' })
+          : res.json(thought)
+      )
       .catch((err) => res.status(500).json(err));
   }
 };
