f4e67f35908b3b05e9a173028e27bdbb29ab15e8
Victoria El Bedewi <victoriaelbedewi@Victorias-Air.askey.com>
2022-10-28 08:45:45 -0700

    get one user

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,7 +12,31 @@ module.exports = {
         res.sendStatus(400);
       });
   },
+ // get one User by id
+ getSingleUser(req, res) {
+    User.findOne({ _id: req.params.id })
+      .populate({
+        path: 'thoughts',
+        select: '-__v'
+      })
+      .populate({
+        path: 'friends',
+        select: '-__v'
+      })
+      .then(User => {
+        if (!User) {
+          res.status(404).json({ message: 'No Users found with this id!' });
+          return;
+        }
+        res.json(User);
+      })
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
 
+ 
  
 };
 
