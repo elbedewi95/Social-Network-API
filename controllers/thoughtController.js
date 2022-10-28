9fdc53a9d23d86eded85ebd4ca43fb6e1638a232
Victoria El Bedewi <victoriaelbedewi@Victorias-Air.askey.com>
2022-10-28 08:15:50 -0700

    find all thoughts

---
+++ controllers/thoughtController.js
@@ -0,0 +1,10 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+  // Get all courses
+  getThoughts(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  }
+};
